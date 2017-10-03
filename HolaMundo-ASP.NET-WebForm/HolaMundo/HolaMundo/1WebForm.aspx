c8c3eb6cb3b76a88616f6f5d575c4037470c001f
11ivan <ivancastillocalle@gmail.com>
2017-10-03 14:32:10 +0200

    up

---
+++ HolaMundo-ASP.NET-WebForm/HolaMundo/HolaMundo/1WebForm.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="1WebForm.aspx.cs" Inherits="HolaMundo._1WebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+                <p>
+                    <asp:Label ID="idNombre" runat="server" Text="Nombre:"></asp:Label>
+                    <asp:TextBox ID="txtNombre" runat="server"></asp:TextBox>
+                    <asp:Label ID="idErrorNombre" runat="server" Text="" ForeColor="Red" Font-Bold="true" Visible="true"></asp:Label>
+                </p>
+                <p>
+                <asp:Label ID="idApellido" runat="server" Text="Apellidos:"></asp:Label>
+                <asp:TextBox ID="txtApellidos" runat="server"></asp:TextBox>
+                <asp:Label ID="idErrorApellido" runat="server" Text="" ForeColor="Red" Font-Bold="true" Visible="true"></asp:Label>
+                </p>
+                <p>
+                    <asp:Button runat="server" Text="Enviar" OnClick="btnEnviar" />
+                </p>
+                <p>
+                    <asp:Label ID="idGood" runat="server" Text="" Visible="true"></asp:Label>
+                    <asp:Calendar ID="Calendar1" runat="server"></asp:Calendar>
+                </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ HolaMundo-ASP.NET-WebForm/HolaMundo/HolaMundo/1WebForm.aspx.designer.cs
@@ -0,0 +1,96 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace HolaMundo {
+    
+    
+    public partial class _1WebForm {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control idNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label idNombre;
+        
+        /// <summary>
+        /// Control txtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+        
+        /// <summary>
+        /// Control idErrorNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label idErrorNombre;
+        
+        /// <summary>
+        /// Control idApellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label idApellido;
+        
+        /// <summary>
+        /// Control txtApellidos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtApellidos;
+        
+        /// <summary>
+        /// Control idErrorApellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label idErrorApellido;
+        
+        /// <summary>
+        /// Control idGood.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label idGood;
+        
+        /// <summary>
+        /// Control Calendar1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Calendar Calendar1;
+    }
+}
