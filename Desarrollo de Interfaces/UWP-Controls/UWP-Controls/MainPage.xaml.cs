926b909685cc5da6b8094dca3d515ef31fa15212
11ivan <ivancastillocalle@gmail.com>
2017-10-22 22:29:41 +0200

    uwp-cpmtrols

--- Desarrollo de Interfaces/UWP-Controls/UWP-Controls/MainPage.xaml.cs
+++ Desarrollo de Interfaces/UWP-Controls/UWP-Controls/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
 
@@ -26,5 +28,69 @@ namespace UWP_Controls
         {
             this.InitializeComponent();
         }
+
+        /*private void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+
+        }
+
+        private void AutoSuggestBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+
+        }
+
+        private void AutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+
+        }*/
+
+        /// <summary>
+        /// When the rectangle is pressed change its color randomly
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ContentHost_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            // Replace the ContentControl's content with a new Rectangle of a random color.
+            Rectangle newItem = new Rectangle();
+            Random rand = new Random();
+
+            newItem.Height = 200;
+            newItem.Width = 200;
+            newItem.Fill = new SolidColorBrush(Color.FromArgb(255,(byte)rand.Next(0, 255), (byte)rand.Next(0, 255), (byte)rand.Next(0, 255)));
+
+            ContentHost.Content = newItem;
+        }
+
+        /// <summary>
+        /// When the Progress Bar is complete, stop the Progress Ring
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ProgressBar_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            if (progressBar.Value==100)
+            {
+                progressRing.IsActive = false;
+            }
+            else
+            {
+                progressRing.IsActive = true;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
