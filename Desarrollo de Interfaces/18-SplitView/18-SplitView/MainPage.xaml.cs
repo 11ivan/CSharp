2769827ab53c149704b9ce689f1bd88fd16b4ead
11ivan <07.qweasd123@gmail.com>
2017-10-26 12:56:26 +0200

    SplitView-DI

---
+++ Desarrollo de Interfaces/18-SplitView/18-SplitView/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace _18_SplitView
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+             split.IsPaneOpen = !split.IsPaneOpen;         
+        }
+
+        private void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtBlockContent.Visibility == Visibility.Visible)
+            {
+                txtBlockContent.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                txtBlockContent.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
