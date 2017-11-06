fe6e12b19df4715f4d431b128c956e60ffee3e48
Unknown <07.qweasd123@gmail.com>
2017-11-06 14:35:03 +0100

    Binding

---
+++ Desarrollo de Interfaces/Binding/Binding/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace Binding
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           
+           
+        }
+
+
+        /// <summary>
+        /// If the key pressed is a number
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key>=VirtualKey.NumberPad0 && e.Key <= VirtualKey.NumberPad9 || e.Key >= VirtualKey.Number0 && e.Key <= VirtualKey.Number9 && !e.Key.Equals(VirtualKeyModifiers.Shift))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+       
+    }
+}
