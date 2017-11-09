fd71a97c119d131251d809ad26c6e8993b65569b
Unknown <07.qweasd123@gmail.com>
2017-11-09 12:55:38 +0100

    DI-BindingDataContext.RIP

---
+++ Desarrollo de Interfaces/19-BindingListaPersonas/19-BindingListaPersonas/Models/Entities/Persona.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _19_BindingListaPersonas.Models
+{
+    public class Persona{
+
+        public int id { get; set; }   
+        //public int idDepartamento { get; set; }
+        public String nombre { get; set; }
+        public String apellido { get; set; }
+        public DateTime fechaNac { get; set; }       
+        public String direccion { get; set; }
+        public String telefono { get; set; }
+
+
+        public Persona()
+        {
+            id = 0;
+            //idDepartamento = 0;
+            nombre = "Iván";
+            apellido = "Castillo";
+            fechaNac = new DateTime(1990, 10, 2);
+            direccion = "C/ S/N";
+            telefono = "955111111";
+        }
+
+        public Persona(int id, String nombre, String apellido, String direccion, String telefono)
+        {
+            this.id = id;
+            //this.idDepartamento = idDepartamento;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.fechaNac = fechaNac;
+            this.direccion = direccion;
+            this.telefono = telefono;
+        }
+
+        public Persona(Persona persona)
+        {
+            this.id = persona.id;
+            //this.idDepartamento = idDepartamento;
+            this.nombre = persona.nombre;
+            this.apellido = persona.apellido;
+             this.fechaNac = fechaNac;
+            this.direccion = persona.direccion;
+            this.telefono = persona.telefono;
+        }
+
+    }
+}
---
+++ Desarrollo de Interfaces/19-BindingListaPersonas/19-BindingListaPersonas/Models/ViewModel/VMMainPage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _19_BindingListaPersonas.Models;
+using _19_BindingListaPersonas.Models.Lists;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace _19_BindingListaPersonas.Models.ViewModel
+{
+    class VMMainPage : INotifyPropertyChanged
+    {
+        private Persona personaSelected;
+        private ListaPersonas listaPersonas;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public VMMainPage()
+        {        
+            listaPersonas = new ListaPersonas();
+        }
+
+
+        public ObservableCollection<Persona> ListaPersonas
+        {
+            get
+            {
+                return listaPersonas.getListaPersonas();
+            }
+        }
+
+        public Persona PersonaSelected
+        {
+            get
+            {
+                return personaSelected;
+            }
+            set
+            {
+                personaSelected = value;
+                NotifyPropertyChanged("PersonaSelected");
+            }
+
+        }
+
+
+        private void NotifyPropertyChanged( String propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+
+
+    }
+}
