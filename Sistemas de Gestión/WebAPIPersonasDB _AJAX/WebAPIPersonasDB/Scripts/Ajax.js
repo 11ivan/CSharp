4ef6fdde9cc98e72e53f66e00c7a2f4f431b50ed
11ivan <ivancastillocalle@gmail.com>
2018-01-17 00:01:30 +0100

    up

--- Sistemas de Gestión/WebAPIPersonasDB _AJAX/WebAPIPersonasDB/Scripts/Ajax.js
+++ Sistemas de Gestión/WebAPIPersonasDB _AJAX/WebAPIPersonasDB/Scripts/Ajax.js
@@ -14,11 +14,13 @@ function inicia() {
 function insertarTabla() {
 
     var xmlhtr = new XMLHttpRequest();
+
     //Creamos la tabla
     var table = document.createElement("table");
 
     if (xmlhtr) {
 
+        //Prepa
         xmlhtr.open('GET', "../api/personas");
 
         xmlhtr.onreadystatechange = function () {
@@ -29,14 +31,21 @@ function insertarTabla() {
                 var arrayPersonas = JSON.parse(xmlhtr.responseText);
 
                 for (var i = 0; i < arrayPersonas.length; i++) {
-                    var persona = new Persona();
-                    persona = arrayPersonas[i];
+                    //var persona = new Persona();
+                    var persona = arrayPersonas[i];
                     table.appendChild(document.createElement("tr"));
 
                     table.childNodes[i].appendChild(document.createElement("td"));
-                    table.childNodes[i].childNodes[0].appendChild(document.createTextNode(persona.nombre));
+                    //table.childNodes[i].childNodes[0].appendChild(document.createTextNode(persona.nombre));
 
-                    Object.keys(persona);
+                    var properties = Object.keys(persona);
+                    for (var j = 0; j < properties.length; j++){
+                        table.childNodes[i].childNodes[j].appendChild(document.createTextNode(persona.Property(j)));
+                        //table.childNodes[i].childNodes[j].textContent = persona[j];
+
+                    }
+
+                    //Object.keys(persona);
 
                     /*for (var j = 0; j < persona.; j++) {
                         table.childNodes[i].appendChild(document.createElement("td"));
@@ -51,43 +60,4 @@ function insertarTabla() {
 }
 
 
-
-function insertaTablaPersonas() {
-    //Creamos la tabla
-    var table = document.createElement("table");
-
-    //Paso 1 instanciar objeto
-    var miPeticion = new XMLHttpRequest();
-
-    //Paso 2 Preparar la Peticion
-    miPeticion.open("GET", "../Server/personas.xml", true);
-
-    //Paso 3 Cabeceras
-
-    //Paso 4 Prepara lo que quiero hacer cuando cambie el estado
-    miPeticion.onreadystatechange = function () {
-        //alert(miPeticion.readyState + "," + miPeticion.status);
-        document.getElementById("divListaPersonas").innerHTML = "Cargando";
-        if (miPeticion.readyState == 4 && miPeticion.status == 200) {
-            //Paso 6 Tratamiento de la informacion cuando se recibe
-            //document.getElementById("divListaPersonas").innerHTML = miPeticion.responseText;
-            var misDatosXML = miPeticion.responseXML;
-            var arrayPersonas = misDatosXML.getElementsByTagName("persona");
-
-            for (var i = 0; i < arrayPersonas.length; i++) {
-                var persona = arrayPersonas[i];
-                table.appendChild(document.createElement("tr"));
-
-                for (var j = 0; j < persona.children.length; j++) {
-                    table.childNodes[i].appendChild(document.createElement("td"));
-                    table.childNodes[i].childNodes[j].appendChild(persona.children[j].cloneNode(false));
-                }
-            }
-            document.getElementById("divTablaPersonas").appendChild(table);
-        }
-    }
-
-    //Paso 5 Enviar la solititud
-    miPeticion.send();
-}
 
--- Sistemas de Gestión/WebAPIPersonasDB _AJAX/WebAPIPersonasDB/Scripts/ModeloJavaScript.js
+++ Sistemas de Gestión/WebAPIPersonasDB _AJAX/WebAPIPersonasDB/Scripts/ModeloJavaScript.js
@@ -9,6 +9,25 @@
         this.telefono = telefono;
     }
 
+
+    /*constructor() {
+        this.id = -1;
+        this.nombre = "";
+        this.apellidos = "";
+        this.fechaNac = "";
+        this.direccion = "";
+        this.telefono = "";
+    }*/
+
+    /*function Persona() {
+        this.id = -1;
+        this.nombre = "";
+        this.apellidos = "";
+        this.fechaNac = "";
+        this.direccion = "";
+        this.telefono = "";
+    }*/
+
     /*get Id() {
         return id;
     }
@@ -26,9 +45,58 @@
     }
 
     get Direccion() {
-        return direccion
+        return direccion;
+    }
+
+
+
+    set Id(value) {
+        this.id = value;
+    }
+
+    set Nombre(value) {
+        this.nombre = value;
+    }
+
+    set Apellidos(value) {
+        this.apellidos = value;
+    }
+
+    set FechaNac(value) {
+        this.fechaNac = value;
+    }
+
+    set Direccion(value) {
+        this.direccion = value;
     }*/
 
-    
+
+    Property(index) {
+        var property;
+        switch (index) {
+            case 0:
+                property = this.id;
+                break;
+
+            case 1:
+                property = this.nombre;
+                break;
+
+            case 2:
+                property = this.apellidos;
+                break;
+
+            case 3:
+                property = this.fechaNac;
+                break;
+
+            case 4:
+                property = this.direccion;
+                break;
+        }
+        return property;
+    }
+
+
 
 }
