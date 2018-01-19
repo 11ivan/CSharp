40a2417879a6b13a47e1856cad9ccefbfa1a7b75
Unknown <07.qweasd123@gmail.com>
2018-01-19 13:00:05 +0100

    up

--- Sistemas de Gestión/WebAPIPersonasDB _AJAX/WebAPIPersonasDB/Scripts/Ajax.js
+++ Sistemas de Gestión/WebAPIPersonasDB _AJAX/WebAPIPersonasDB/Scripts/Ajax.js
@@ -76,14 +76,21 @@ function insertarTabla() {
                     table.childNodes[i+1].appendChild(document.createElement("td"));
                     table.childNodes[i+1].childNodes[5].appendChild(document.createTextNode(persona.direccion));
 
+                    //Botones
+                    table.childNodes[i + 1].appendChild(document.createElement("td"));
+                    var buttonCreate = document.createElement("button");
+                    buttonCreate.setAttribute("tag", i);
+                    buttonCreate.setAttribute("value", persona.id);
+                    buttonCreate.innerHTML = "Editar";
+                    buttonCreate.addEventListener("click", editarPersona, true); 
+                    table.childNodes[i + 1].childNodes[6].appendChild(buttonCreate);
+
                     /*var properties = Object.keys(persona);
                     for (var j = 0; j < properties.length; j++){
                         table.childNodes[i].childNodes[j].appendChild(document.createTextNode(persona.Property(j)));
                         //table.childNodes[i].childNodes[j].textContent = persona[j];
 
                     }*/
-
-                    //Object.keys(persona);
 
                     /*for (var j = 0; j < persona.; j++) {
                         table.childNodes[i].appendChild(document.createElement("td"));
@@ -97,5 +104,46 @@ function insertarTabla() {
     xmlhtr.send();
 }
 
+function editarPersona() {
+    //alert(this.value + " " + this.getAttribute("tag"));
+    var xmlhtr = new XMLHttpRequest();
+    var fila = this.getAttribute("tag");
+    var id = this.value;
 
+    if (xmlhtr) {
+
+        xmlhtr.open('GET', "../api/personas/"+id);
+
+        xmlhtr.onreadystatechange = function () {
+            if (xmlhtr.readyState == 4 && xmlhtr.status == 200) {
+                //document.getElementById("listadoPersonas").innerHTML = xmlhtr.responseText;//
+               
+                //Deserializacion del documento JSON
+                var persona = new Persona();
+                persona = JSON.parse(xmlhtr.responseText);                  
+
+                table.childNodes[fila].appendChild(document.createElement("td"));//+1 porque el iondice 0 esta ocupado
+                table.childNodes[fila].childNodes[0].appendChild(document.createTextNode(persona.id));
+
+                table.childNodes[fila].appendChild(document.createElement("td"));
+                table.childNodes[fila].childNodes[1].appendChild(document.createTextNode(persona.nombre));
+
+                table.childNodes[fila].appendChild(document.createElement("td"));
+                table.childNodes[fila].childNodes[2].appendChild(document.createTextNode(persona.apellidos));
+
+                table.childNodes[fila].appendChild(document.createElement("td"));
+                table.childNodes[fila].childNodes[3].appendChild(document.createTextNode(persona.fechaNac.split("T", 1)));
+
+                table.childNodes[fila].appendChild(document.createElement("td"));
+                table.childNodes[fila].childNodes[4].appendChild(document.createTextNode(persona.telefono));
+
+                table.childNodes[fila].appendChild(document.createElement("td"));
+                table.childNodes[fila].childNodes[5].appendChild(document.createTextNode(persona.direccion));
+
+                document.getElementById("listadoPersonas").appendChild(table);
+            }
+        }
+    }
+    xmlhtr.send();
+}
 
